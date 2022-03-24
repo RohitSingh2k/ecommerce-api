37c960d8f0d8d62dedc456cf15bc697ad2cd9f65
Rohit Singh <singh.rohitsingh2k@gmail.com>
2022-03-24 18:31:07 +0530

    refactored code.

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require('path');
 const dotenv = require("dotenv");
-const userRoute = require("./routes/user");
+const express = require("express");
+const mongoose = require("mongoose");
+
+// All routes are here
 const authRoute = require("./routes/auth");
-const productRoute = require("./routes/product");
+const userRoute = require("./routes/user");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
 const stripeRoute = require("./routes/stripe");
-const cors = require("cors");
+const productRoute = require("./routes/product");
 
+const app = express();
+
+// configuring environment variables.
 dotenv.config();
 
+// configuring database connection.
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("DB Connection Successfull!"))
@@ -19,8 +25,13 @@ mongoose
     console.log(err);
   });
 
+
+// Using middleware here.
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')))
+
+// initialising routes of the APIs.
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
