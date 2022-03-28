4f11cdcced90d61fc6785a98398fa0323bb4bf30
Rohit Singh <singh.rohitsingh2k@gmail.com>
2022-03-28 16:27:05 +0530

    worked on stripe integeration.

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,21 +1,37 @@
 const router = require("express").Router();
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 
-router.post("/payment", (req, res) => {
-  stripe.charges.create(
-    {
-      source: req.body.tokenId,
-      amount: req.body.amount,
-      currency: "usd",
-    },
-    (stripeErr, stripeRes) => {
-      if (stripeErr) {
-        res.status(500).json(stripeErr);
-      } else {
-        res.status(200).json(stripeRes);
-      }
-    }
-  );
+const items = new Map([
+  [1, {priceInRupees : 100, name : "something"}],
+  [2, {priceInRupees : 500, name : "second_something"}]
+])
+
+router.post("/payment", async (req, res) => {
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types:["card"],
+      mode: 'payment',
+      line_items : req.body.items.map( item => {
+        const storeItem = items.get(item.id)
+        return {
+          price_data : {
+            currency : "inr",
+            product_data:{
+              name : storeItem.name
+            },
+            unit_amount : storeItem.priceInRupees
+          },
+          quantity : item.quantity
+        }
+      }),
+      success_url: `${process.env.BASE_URL}/success.html`,
+      cancel_url: `${process.env.BASE_URL}/cancel.html`,
+    });
+  } catch (e) {
+    console.log(e);
+  }
+  res.redirect(303, session.url);
 });
 
 module.exports = router;
