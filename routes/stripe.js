ea4a434bd3c177b557c2b9e9bffb5b5a2103a8b6
Rohit Singh <singh.rohitsingh2k@gmail.com>
2022-03-28 20:04:07 +0530

    payment gateway integrated successfully [stripe].

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const uuid = require('uuid');
 
 const items = new Map([
   [1, {priceInRupees : 100, name : "something"}],
@@ -8,30 +10,30 @@ const items = new Map([
 
 router.post("/payment", async (req, res) => {
 
-  try {
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types:["card"],
-      mode: 'payment',
-      line_items : req.body.items.map( item => {
-        const storeItem = items.get(item.id)
-        return {
-          price_data : {
-            currency : "inr",
-            product_data:{
-              name : storeItem.name
-            },
-            unit_amount : storeItem.priceInRupees
-          },
-          quantity : item.quantity
-        }
-      }),
-      success_url: `${process.env.BASE_URL}/success.html`,
-      cancel_url: `${process.env.BASE_URL}/cancel.html`,
-    });
-  } catch (e) {
-    console.log(e);
-  }
-  res.redirect(303, session.url);
+  const {product, token} = req.body;
+  const idempotencyKey = uuid();
+
+  return stripe.customers.create({
+    email : token.eamil,
+    source : token.id
+  }).then( customer => {
+    stripe.charge.create({
+      amount : product.price * 100,
+      currency : 'inr',
+      customer : customer.id,
+      receipt_email : token.email,
+      description : `Purchasing ${product.name}`,
+      shipping : {
+        name : token.card.name,
+        address : {
+          country : token.card.address_country 
+        } 
+      }
+    },
+    {idempotencyKey})
+  })
+  .then( result => res.status(200).json(result))
+  .catch( err => console.log(err))
 });
 
 module.exports = router;
