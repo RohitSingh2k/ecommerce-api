7b08628f36d7fe2e74ba4e095a015c65535fd784
Rohit Singh <singh.rohitsingh2k@gmail.com>
2022-03-29 14:27:11 +0530

    modified user model.

--- models/User.js
+++ models/User.js
@@ -2,8 +2,10 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
   {
+    name :{type:String, required:true},
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     isAdmin: {
       type: Boolean,
